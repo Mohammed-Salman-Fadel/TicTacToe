c35b362ea6b0eff83f910b2814483aa0887a5ad2
Mohammed-Salman-Fadel <126667773+Mohammed-Salman-Fadel@users.noreply.github.com>
2023-03-07 19:52:06 +0200

    Code for Game

--- main.c
+++ main.c
@@ -1,17 +1,182 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 
-int func(int A[])
+
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+
+int main()
 {
-    int size=sizeof(A)/sizeof(A[0]);
-    for(int i=0;i<5;i++)
+    char winner =' ';
+    resetBoard();
+
+    while(winner == ' ' && checkFreeSpaces != 0)
     {
-        printf("%d",A[i]);
+      printBoard();
+
+      playerMove();
+      winner=checkWinner();
+
+      if(winner != ' ' || checkFreeSpaces == 0)
+      {
+        break;
+      }
+      computerMove();
+      winner=checkWinner();
+
+      if(winner != ' ' || checkFreeSpaces == 0)
+      {
+        break;
+      }
+
+    }
+
+    printBoard();
+    printWinner(winner);
+
+
+    return 0;
+}
+
+
+void resetBoard()
+{
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            board[i][j]=' ';
+        }
     }
 }
 
-int main(void)
+void printBoard()
 {
-    int A[]={1,2,3,4,5};
-    func(&A);
+    printf(" %c | %c | %c ",board[0][0],board[0][1], board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[1][0],board[1][1], board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ",board[2][0],board[2][1], board[2][2]);
+    printf("\n");
 }
 
+int checkFreeSpaces()
+{
+    int freeSpaces=9;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            if(board[i][j] != ' ')
+            {
+                freeSpaces--;
+            }
+        }
+    }
+    return freeSpaces;
+}
+
+void playerMove()
+{
+    int x, y;
+    printf("Enter the number of Row: ");
+    scanf("%d",&x);
+    printf("Enter ther number of Coloum: ");
+    scanf("%d",&y);
+    x--;
+    y--;
+    do
+    {
+    if(board[x][y] != ' ')
+        {
+        printf("Invalid Move\n");
+        }
+        else{
+        board[x][y]=PLAYER;
+        break;
+        }
+    }while(board[x][y] != ' ');
+
+
+}
+
+void computerMove()
+{
+    srand(time(0));
+    int x;
+    int y;
+
+    if(checkFreeSpaces() > 0)
+    {
+        do{
+            x=rand() % 3;
+            y=rand() % 3;
+        }while(board[x][y] != ' ');
+
+        board[x][y]=COMPUTER;
+    }
+    else
+    {
+        printWinner(' ');
+    }
+
+
+}
+
+char checkWinner()
+{
+    //For Rows
+    for(int i=0;i<3;i++)
+    {
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
+        {
+            return board[i][0];
+        }
+    }
+    //For coloums
+    for(int i=0;i<3;i++)
+    {
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+    //For Diagonals
+        if(board[0][0] == board[1][1] && board[0][0] == board[2][2])
+        {
+            return board[0][0];
+        }
+        if(board[0][2] == board[1][1] && board[0][0] == board[2][0])
+        {
+            return board[0][2];
+        }
+    return ' ';
+}
+void printWinner(char winner)
+{
+    if(winner == PLAYER)
+    {
+        printf("You Win!!");
+    }
+    else if(winner == COMPUTER)
+    {
+        printf("You Lose!! )");
+    }
+    else
+    {
+        printf("It's a Draw!");
+    }
+}
